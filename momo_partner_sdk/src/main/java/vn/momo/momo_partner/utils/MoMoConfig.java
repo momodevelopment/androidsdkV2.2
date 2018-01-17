8cbb0db6b7aafd74d9e3311132cca64445b0e0a9
momodevelopment <lanhluu.vn@gmail.com>
2018-01-17 10:20:28 +0700

    Update pakage on Store

--- momo_partner_sdk/src/main/java/vn/momo/momo_partner/utils/MoMoConfig.java
+++ momo_partner_sdk/src/main/java/vn/momo/momo_partner/utils/MoMoConfig.java
@@ -5,10 +5,11 @@ package vn.momo.momo_partner.utils;
  */
 
 public class MoMoConfig {
-    public static final String MOMO_APP_PAKAGE_CLASS_PRODUCTION = "com.mservice";
-    public static final String MOMO_APP_PAKAGE_CLASS_DEBUG = "com.mservice";
-    public static final String MOMO_APP_PAKAGE_CLASS_DEVELOPER = "com.mservice";
     public static final String MOMO_APP_PAKAGE_STORE_DOWNLOAD = "com.mservice.momotransfer";
+
+    public static final String MOMO_APP_PAKAGE_CLASS_PRODUCTION = "com.mservice.momotransfer"; //production
+    public static final String MOMO_APP_PAKAGE_CLASS_DEBUG = "com.mservice.debug";//debug
+    public static final String MOMO_APP_PAKAGE_CLASS_DEVELOPER = "com.mservice";//dev
 
     public static final String ACTION_SDK = "com.android.momo.SDK";//action mapping
     public static final String ACTION_PAYMENT = "com.android.momo.PAYMENT";//action payment
